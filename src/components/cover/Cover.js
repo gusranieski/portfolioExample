397819e0c220da2df7852499270c5a440f944518
gustavo.rañieski <gustavoranieski@hotmail.com>
2022-11-08 00:40:02 -0300

    componente cover y navbar

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import "./Cover.css";
+import coverVideo from "../../media/coverVideo.mp4";
 
 const Cover = () => {
   return (
-    <div>Cover</div>
-  )
-}
+    <div className="cover-container">
+      <video className="video" src={coverVideo} autoPlay loop muted />
+      <h1>Gustavo Rañieski</h1>
+      <p>Frontend Developer</p>
+    </div>
+  );
+};
 
-export default Cover
+export default Cover;
